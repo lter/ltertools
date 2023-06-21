d92ec3267f1e067727e5451a3c817cd29a32801c
njlyon0 <nicholasjlyon@gmail.com>
2023-06-21 12:18:56 -0400

    Adding first pass of conversion function to package documentation (with `devtools::document`)

--- R/unit_convert.R
+++ R/unit_convert.R
@@ -1,4 +1,22 @@
-
+#' @title Convert Units of a Value
+#' 
+#' @description Converts a given set of values from one user-specified unit to another. Currently only supports inter-conversion of temperature (i.e., Kelvin, Fahrenheit, Celsius) or miles to kilometers. If you would like other units added, please post an issue on our GitHub repository with your requested units and the formula used to convert between them.
+#' 
+#' @param value (numeric) value to convert between
+#' @param from (character) starting units of the value. Casing (upper vs. lower) does not matter though spelling does
+#' @param to (character) units to which to convert. Again, casing does not affect the function
+#' 
+#' @return (numeric) value converted from specified units to desired units
+#' 
+#' @export
+#' 
+#' @examples
+#' # Convert from Fahrenheit to Celsius
+#' convert(value = 32, from = "Fahrenheit", to = "c")
+#' 
+#' # Convert from kilometers to miles
+#' convert(value = 5, from = "km", to = "mi")
+#' 
 convert <- function(value = NULL, from = NULL, to = NULL){
   
   # Error out if any are NULL
@@ -9,16 +27,21 @@ convert <- function(value = NULL, from = NULL, to = NULL){
   if(is.numeric(value) != TRUE)
     stop("Value to convert must be numeric")
   
+  # Error out if either from or to is not a character
+  if(is.character(from) != TRUE | is.character(to) != TRUE)
+    stop("Units to convert between (`from` and `to` arguments) must be specified as characters")
+  
   # Coerce from/to into lowercase (less variation to account for later)
   from_low <- tolower(x = from)
   to_low <- tolower(x = to)
   
   # Identify supported units
-  valid_units <- c("c", "celsius", "f", "fahrenheit", "k", "kelvin")
+  valid_units <- c("c", "celsius", "f", "fahrenheit", "k", "kelvin",
+                   "km", "kilometers", "mi", "miles")
   
   # Error out if units are unknown
-  if(!from %in% valid_units | !to %in% valid_units)
-    stop(paste0("Units not supported. Please use one of ", valid_units))
+  if(!from_low %in% valid_units | !to_low %in% valid_units)
+    stop("Units not supported. Please use one of ", paste(valid_units, collapse = "; "))
   
   # Temperature ----
   
@@ -52,19 +75,23 @@ convert <- function(value = NULL, from = NULL, to = NULL){
     converted <- ((value - 273.15) * (9 / 5)) + 32
   }
   
+  # Distance ----
   
+  # miles to kilometers conversion
+  if(from_low %in% c("mi", "miles") & to_low %in% c("km", "kilometers")){
+    converted <- value * 1.609344
+  }
   
+  # kilometers to miles conversion
+  if(from_low %in% c("km", "kilometers") & to_low %in% c("mi", "miles")){
+    converted <- value * 0.621371
+  }
   
-  
-  # Template conditional for future conversions
+  # Template Conditional  ----
+  ## For future conversions
   # if(from_low %in% c() & to_low %in% c()){
   #   converted <- value
   # }
   
-  # Return Conversion ----
-  return(converted)
-  
-}
-
-
-
+  # Return Converted value ----
+  return(converted) }
