14af1e3c99b2d8d0a3d37b638a9218600b1f3930
njlyon0 <nicholasjlyon@gmail.com>
2023-07-05 12:04:51 -0400

    Adding `roxygen2` formatting for function

--- R/cv.R
+++ R/cv.R
@@ -1,6 +1,18 @@
-
-
-
+#' @title Calculate Coefficient of Variation
+#' 
+#' @description Computes the coefficient of variation (CV), by dividing the standard deviation (SD) by the arithmetic mean of a set of numbers. If `na_rm` is `TRUE` then missing values are removed before calculation is completed
+#' 
+#' @param x (numeric) vector of numbers for which to calculate CV
+#' @param na_rm (logical) whether to remove missing values from both average and SD calculation
+#' 
+#' @return (numeric) coefficient of variation
+#' 
+#' @export
+#' 
+#' @examples
+#' # Convert from Fahrenheit to Celsius
+#' cv(x = c(4, 5, 6, 4, 5, 5), na_rm = TRUE)
+#' 
 cv <- function(x, na_rm = TRUE){
   
   # Error out if X is not numeric
