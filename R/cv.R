06d39cee16bc17ef824c29a33c32942655977b99
njlyon0 <nicholasjlyon@gmail.com>
2023-07-05 11:58:22 -0400

    Namespace `stats` for SD / mean

--- R/cv.R
+++ R/cv.R
@@ -14,7 +14,7 @@ cv <- function(x, na_rm = TRUE){
   }
   
   # Calculate CV
-  result <- sd(x = x, na.rm = na_rm) / mean(x = x, na.rm = na_rm)
+  result <- stats::sd(x = x, na.rm = na_rm) / stats::mean(x = x, na.rm = na_rm)
   
   # Return it
   return(result) }
