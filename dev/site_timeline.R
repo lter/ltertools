8fadfe1671c4d8a27c95e79195c54abe44d7f1ec
njlyon0 <nicholasjlyon@gmail.com>
2023-08-23 12:42:40 -0400

    Removed unwanted vertical line and export step from previous use of this timeline code

--- dev/site_timeline.R
+++ dev/site_timeline.R
@@ -19,8 +19,6 @@ habitat_colors <- c("Admin" = "#fcbf49",
 ggplot(site_long, aes(x = year, y = factor(code, levels = lter_sites$code))) +
   geom_path(aes(group = code, color = habitat), lwd = 1.5, lineend = 'round') +
   geom_point(aes(fill = habitat), pch = 21, size = 3) +
-  # Vertical line at current year
-  geom_vline(xintercept = )
   # Custom color
   scale_fill_manual(values = habitat_colors) +
   scale_color_manual(values = habitat_colors) +
@@ -31,14 +29,4 @@ ggplot(site_long, aes(x = year, y = factor(code, levels = lter_sites$code))) +
         axis.title = element_blank(),
         axis.text = element_text(size = 12),
         legend.title = element_blank())
-
-# Export this graph
-# ggsave(plot = timeline, filename = file.path("plots", "LTER_site_timeline.png"),
-#        height = 5, width = 6, units = "in")
-
-
-
-ggplot(site_long, aes(x = year, y = factor(code, levels = lter_sites$code))) +
-  geom_path(aes(group = code, color = habitat), lwd = 1, lineend = 'round') +
-  geom_point(aes(fill = habitat), pch = 21, size = 2)
 
