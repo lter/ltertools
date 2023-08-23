5623de81b554fd20a9ede6b929b7c939c4009839
njlyon0 <nicholasjlyon@gmail.com>
2023-08-23 12:40:49 -0400

    Fidgeting with timeline aesthetics / color palette

--- dev/site_timeline.R
+++ dev/site_timeline.R
@@ -5,28 +5,32 @@ site_long <- lter_sites %>%
 
 
 # Get vector of habitat colors
-habitat_colors <- c("Coastal" = "#34a0a4", 
-                    "Freshwater" = "#48cae4", 
-                    "Marine" = "#1e6091", 
-                    "Forest" = "#007200",
+habitat_colors <- c("Admin" = "#fcbf49",
+                    "Coastal" = "#34a0a4", 
+                    "Freshwater" = "#ade8f4", 
+                    "Marine" = "#023e8a", 
+                    "Forest" = "#55a630",
                     "Grassland" = "#9ef01a", 
-                    "Mixed Landscape" = "#7f5539",
+                    "Mixed" = "#9d4edd",
                     "Tundra" = "#bb9457", 
-                    "Urban" = "#9d4edd")
+                    "Urban" = "#f77f00")
 
 # Make timeline
-timeline <- ggplot(site_long, aes(x = year, y = code)) +
-  geom_path(aes(group = code, color = habitat), lwd = 1, lineend = 'round') +
-  geom_point(aes(fill = habitat), pch = 21, size = 2) +
+ggplot(site_long, aes(x = year, y = factor(code, levels = lter_sites$code))) +
+  geom_path(aes(group = code, color = habitat), lwd = 1.5, lineend = 'round') +
+  geom_point(aes(fill = habitat), pch = 21, size = 3) +
+  # Vertical line at current year
+  geom_vline(xintercept = )
   # Custom color
   scale_fill_manual(values = habitat_colors) +
   scale_color_manual(values = habitat_colors) +
   # Customize theme elements
-  theme_bw() +
   theme(panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(color = "black"),
         axis.title = element_blank(),
         axis.text = element_text(size = 12),
-        legend.title = element_blank()); timeline
+        legend.title = element_blank())
 
 # Export this graph
 # ggsave(plot = timeline, filename = file.path("plots", "LTER_site_timeline.png"),
