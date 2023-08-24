df138718a2a2a7246adbf2fa47382f4bae10f733
njlyon0 <nicholasjlyon@gmail.com>
2023-08-24 09:51:47 -0400

    First pass at roxygen formatting for `site_timeline`

--- R/site_timeline.R
+++ R/site_timeline.R
@@ -1,21 +1,31 @@
-
-
-
-
-
-
-
-
-# Actually generate timeline graph
-
-
-lter_timeline <- function(sites = NULL, habitats = NULL, colors = NULL){
+#' @title Create a Timeline of Site(s) that Meet Criteria
+#' 
+#' @description Creates a ggplot2 plot of all sites that meet the user-specified site code (i.e., three letter abbreviation) and/or habitat criteria. See `lter_sites` for the full set of site information including accepted site codes and habitat designations (unrecognized entries will trigger a warning and be ignored). Lines are grouped and colored by habitat to better emphasize possible similarities among sites. This function was built by the following authors: Nicholas Lyon
+#' 
+#' @param sites (character) three letter site code(s) identifying site(s) of interest
+#' @param habitats (character) habitat(s) of interest. See `unique(lter_sites$habitat)`
+#' @param colors (character) 
+#' 
+#' @return (ggplot2) plot object of timeline of site(s) that meet user-specified criteria
+#' 
+#' @export
+#' 
+#' @examples
+#' # Make the full timeline of all sites with default colors by supplying no arguments
+#' site_timeline()
+#' 
+#' # Or make a timeline of only sites that meet certain criteria
+#' site_timeline(habitats = c("gassland", "forest"))
+#' 
+site_timeline <- function(sites = NULL, habitats = NULL, colors = NULL){
+  # Squelch visible bindings NOTE
+  year <- code <- habitat <- NULL
   
   # Subset to just those sites / habitats
-  sites_sub <- ltertools:::site_subset(sites = sites, habitats = habitats)
+  sites_sub <- site_subset(sites = sites, habitats = habitats)
   
   # Pivot to long format
-  sites_long <- tidyr::pivot_longer(data = sites_sub, cols = ends_with("_year"), 
+  sites_long <- tidyr::pivot_longer(data = sites_sub, cols = dplyr::ends_with("_year"), 
                                     names_to = "cols", values_to = "year")
   
   # Define default colors
@@ -25,26 +35,27 @@ lter_timeline <- function(sites = NULL, habitats = NULL, colors = NULL){
                       "Mixed" = "#9d4edd", "Tundra" = "#bb9457")
   
   # Make initial timeline graph
-  times_v1 <- ggplot(sites_long, aes(x = year, y = factor(code, levels = sites_sub$code))) +
+  times_v1 <- ggplot2::ggplot(sites_long, ggplot2::aes(x = year, 
+                                              y = factor(code, levels = sites_sub$code))) +
     # Lines for timeline
-    geom_path(aes(group = code, color = habitat), lwd = 1.5, lineend = 'round') +
+    ggplot2::geom_path(ggplot2::aes(group = code, color = habitat), lwd = 1.5, lineend = 'round') +
     # Start / end points of timeline
-    geom_point(aes(fill = habitat), pch = 21, size = 3) +
+    ggplot2::geom_point(ggplot2::aes(fill = habitat), pch = 21, size = 3) +
     # Customize theme elements
-    theme(panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.line = element_line(color = "black"),
-          axis.title = element_blank(),
-          axis.text = element_text(size = 12),
-          legend.title = element_blank())
+    ggplot2::theme(panel.border = ggplot2::element_blank(),
+          panel.background = ggplot2::element_blank(),
+          axis.line = ggplot2::element_line(color = "black"),
+          axis.title = ggplot2::element_blank(),
+          axis.text = ggplot2::element_text(size = 12),
+          legend.title = ggplot2::element_blank())
   
   # If the user provided colors that are the correct length
   if(is.null(colors) != TRUE & length(colors) >= length(unique(sites_long$habitat))){
     
     # Use those custom colors
     times_v2 <- times_v1 +
-      scale_fill_manual(values = colors) +
-      scale_color_manual(values = colors) }
+      ggplot2::scale_fill_manual(values = colors) +
+      ggplot2::scale_color_manual(values = colors) }
   
   # If palette is provided but isn't long enough for habitats in the data object
   if(is.null(colors) != TRUE & length(colors) < length(unique(sites_long$habitat))){
@@ -54,22 +65,15 @@ lter_timeline <- function(sites = NULL, habitats = NULL, colors = NULL){
     
     # Use the default colors
     times_v2 <- times_v1 +
-      scale_fill_manual(values = habitat_colors) +
-      scale_color_manual(values = habitat_colors) }
+      ggplot2::scale_fill_manual(values = habitat_colors) +
+      ggplot2::scale_color_manual(values = habitat_colors) }
   
   # If no palette is provided, just use the default colors (no error message)
   if(is.null(colors) == TRUE){
     
     times_v2 <- times_v1 +
-      scale_fill_manual(values = habitat_colors) +
-      scale_color_manual(values = habitat_colors) }
+      ggplot2::scale_fill_manual(values = habitat_colors) +
+      ggplot2::scale_color_manual(values = habitat_colors) }
   
   # Return that object
   return(times_v2) }
-
-
-lter_timeline()
-
-lter_timeline(habitats = c("marine", "ocean", "coastal"))
-
-
