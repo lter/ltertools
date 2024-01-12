0f103339823977601d830d9b1a8b73b78181e70d
njlyon0 <nicholasjlyon@gmail.com>
2024-01-12 10:24:22 -0500

    Created script to test function variant of 'data key' harmonization

---
+++ dev/harmonize.R
@@ -0,0 +1,19 @@
+## ----------------------------------- ##
+# Update LTER Site Info
+## ----------------------------------- ##
+
+# Purpose:
+## Explore possibility of function variant of 'data key'-based harmonization workflow
+
+## ----------------------------------- ##
+# Housekeeping ----
+## ----------------------------------- ##
+# Clear environment / collect garbage
+rm(list = ls()); gc()
+
+
+
+# Clear environment / collect garbage
+rm(list = ls()); gc()
+
+# End ----
