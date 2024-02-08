e7006f3477a8958fedd305970daccdc35999cde3
njlyon0 <nicholasjlyon@gmail.com>
2024-02-08 11:47:43 -0500

    Changed `begin_key` to use `read` for its earliest phase

--- dev/begin_key.R
+++ dev/begin_key.R
@@ -5,8 +5,8 @@
 # Purpose:
 ## Support "data key"-based harmonization workflow by testing a function that can create (the start of) the column key
 
-# Clear environment / collect garbage
-rm(list = ls()); gc()
+# Clear environment
+rm(list = ls())
 
 # Load libraries
 librarian::shelf(devtools, tidyverse)
@@ -15,49 +15,47 @@ librarian::shelf(devtools, tidyverse)
 devtools::load_all()
 
 # Define function
-begin_key <- function(raw_folder = NULL){
-  # raw_folder <- "dev"
+begin_key <- function(raw_folder = NULL, data_format = c("csv", "txt", "xls", "xlsx")){
+  # raw_folder = file.path("dev", "testing")
+  # data_format = c("csv", "txt", "xls", "xlsx")
   
-  # List files ending in CSV in the supplied directory
-  raw_files <- dir(path = file.path(raw_folder), pattern = ".csv")
+  # Read in all files in folder of specified type
+  df_list <- ltertools::read(raw_folder = raw_folder, data_format = data_format)
   
-  # Error out if no CSVs are in that folder
-  if(length(raw_files) == 0)
-    stop("No CSV files detected in supplied path")
+  # Error out if no files are in that folder
+  if(length(df_list) == 0)
+    stop("No files of supported file types found in supplied path")
   
   # Make an empty list
-  file_list <- list()
+  key_list <- list()
   
-  # Loop across identified files
-  for(file in raw_files){
+  # Loop across elements of the list provided by `read`
+  for(g in 1:length(df_list)){
     
-    # Read it in
-    df <- read.csv(file = file.path(raw_folder, file))
+    # Identify file name
+    file_name <- names(df_list)[g]
     
-    # Grab the column names
-    df_names <- names(x = df)
+    # Identify the column names within that file
+    col_names <- names(df_list[[g]])
     
-    # Assemble this facet of the data key
-    key_sub <- data.frame("source" = file,
-                          "raw_name" = df_names,
-                          "tidy_name" = NA)
-    
-    # Add that to the file list
-    file_list[[file]] <- key_sub }
+    # Assemble this facet of the column key and add to the list
+    key_list[[file_name]] <- data.frame("source" = file_name,
+                                        "raw_name" = col_names,
+                                        "tidy_name" = NA) }
+  # Unlist into a dataframe
+  key_df <- purrr::list_rbind(x = key_list)
   
-  # Unlist the file list
-  key <- purrr::list_rbind(x = file_list)
-  
-  # Return that dataframe
-  return(key) }
+  # Return this key skeleton
+  return(key_df) }
 
 # Invoke function
-fxn_out <- begin_key(raw_folder = "dev")
+fxn_out <- begin_key(raw_folder = file.path("dev", "testing"),
+                     data_format = c("csv", "txt", "xls", "xlsx"))
 
 # Look at output
 fxn_out
 
-# Clear environment / collect garbage
-rm(list = ls()); gc()
+
+
 
 # End ----
