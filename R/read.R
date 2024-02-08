c22c7955816778e9da88af529f2612bbb819a73f
njlyon0 <nicholasjlyon@gmail.com>
2024-02-08 11:04:44 -0500

    First pass at roxygen-format for `read` function

--- R/read.R
+++ R/read.R
@@ -1,107 +1,20 @@
-## ----------------------------------- ##
-          # 'Read' Testing ----
-## ----------------------------------- ##
-
-# Purpose:
-## Both `harmonize` and `begin_key` need to read in every data file in a user-defined folder
-## This operation needs to be pretty flexible to handle different data types, so it makes sense to create a helper function to do this that both of those functions can then invoke to read in the desired data
-## Future updates to supported data types can then be made here and will automatically affect the downstream functions without the need for semi-duplicated revisions
-
-## ----------------------------------- ##
-            # Housekeeping ----
-## ----------------------------------- ##
-# Load libraries
-librarian::shelf(devtools, tidyverse, readxl)
-
-# Clear environment / collect garbage
-rm(list = ls())
-
-## ----------------------------------- ##
-          # Script Variant ----
-## ----------------------------------- ##
-# Initial reading in _script format_
-
-# Make an object for the focal directory
-data_folder <- file.path("dev", "testing")
-
-# Create vector of file extensions to look for
-sufx <- c(".csv", ".txt", ".xlsx", ".xls")
-
-# Make an empty list
-file_list <- list()
-
-# Loop across these identifying files of each type in a particular folder
-for(ext in sufx){
-  
-  # Check for files of that type in the folder
-  found_vec <- dir(path = data_folder, pattern = ext)
-  
-  # For the check of ".xls" files we need to do an additional step
-  if(ext == ".xls"){
-    
-    # Identify any modern Excel files (.xlsx) were found
-    modern_excel <- stringr::str_detect(string = found_vec, pattern = ".xlsx")
-    
-    # And remove them (they'll be found by the dedicated check for their file extension)
-    found_vec <- setdiff(x = found_vec, y = found_vec[modern_excel]) }
-  
-  # Create a simple dataframe for storing this information
-  found_df <- data.frame("name" = found_vec,
-                         "type" = ext)
-  
-  # Add to the list
-  file_list[[ext]] <- found_df }
-
-# Unlist the list
-file_df <- purrr::list_rbind(x = file_list)
-
-# check that out
-file_df
-
-# Create a new list
-data_list <- list()
-
-# Loop across rows in the dataframe produced above
-for(k in 1:nrow(file_df)){
-  
-  # CSV Files
-  if(file_df[k,]$type %in% c(".csv")){
-    # Read in
-    data <- read.csv(file = file.path(data_folder, file_df[k,]$name))
-    
-    # Add to list
-    data_list[[file_df[k,]$name]] <- data }
-  
-  # TXT files
-  if(file_df[k,]$type %in% c(".txt")){
-    # Read in
-    data <- read.delim(file = file.path(data_folder, file_df[k,]$name))
-    
-    # Add to list
-    data_list[[file_df[k,]$name]] <- data }
-  
-  # Microsoft Excel files
-  if(file_df[k,]$type %in% c(".xls", ".xlsx")){
-    # Read in
-    data <- readxl::read_excel(path = file.path(data_folder, file_df[k,]$name))
-    
-    # Add to list
-    data_list[[file_df[k,]$name]] <- data }
-  
-} # Close loop
-
-# Check out resulting list
-str(data_list)
-
-# Re-clear environment
-rm(list = ls())
-
-## ----------------------------------- ##
-          # Function Variant ----
-## ----------------------------------- ##
-# Initial reading in _function format_
-
-# Define function
+#' @title Read Data from Folder
+#' 
+#' @description Reads in all data files of specified types found in the designated folder. Returns a list with one element for each data file. Currently supports CSV, TXT, XLS, and XLSX. This function was built by the following authors: Nicholas Lyon
+#' 
+#' @param raw_folder (character) folder / folder path containing data files to read
+#' @param data_formats (character) file extensions to identify within the `raw_folder`. Default behavior is to search for all supported file types.
+#' 
+#' @return (list) data found in specified folder / of specified file format
+#' 
+#' @export
+#' 
+#' @examples
+#' \dontrun{
+#' # Read in all CSV files in the "raw_data" folder
+#' df_list <- read(raw_folder = "raw_data", data_formats = "csv")
+#' }
+#' 
 read <- function(raw_folder = NULL, data_formats = c("csv", "txt", "xls", "xlsx")){
   # raw_folder <- file.path("dev", "testing")
   # data_formats <- c(".csv", ".txt", ".xls", ".xlsx", "xlsx")
@@ -110,7 +23,17 @@ read <- function(raw_folder = NULL, data_formats = c("csv", "txt", "xls", "xlsx"
   if(is.null(x = raw_folder) == TRUE)
     stop("Raw folder must be specified")
   
-  # Make sure periods are not included in file extensions and drop non-unique entries
+  # # Check for unsupported file types
+  # bad_ext <- setdiff(x = data_formats, y = c("csv", "txt", "xls", "xlsx"))
+  # 
+  # # Warn / skip unsupported file types
+  # if(length(bad_ext) != 0){
+  #   
+  #   message()
+  #   
+  # }
+
+    # Make sure periods are not included in file extensions and drop non-unique entries
   formats <- unique(gsub(pattern = "\\.", replacement = "", x = data_formats))
   
   # Make an empty list
@@ -118,8 +41,7 @@ read <- function(raw_folder = NULL, data_formats = c("csv", "txt", "xls", "xlsx"
   
   # Loop across user-supplied file extensions
   for(ext in formats){
-    # ext <- "xls"
-    
+
     # Check for files of that type in the folder
     found_vec <- dir(path = raw_folder, pattern = paste0(".", ext))
     
@@ -176,14 +98,3 @@ read <- function(raw_folder = NULL, data_formats = c("csv", "txt", "xls", "xlsx"
   
   # Return that list
   return(data_list) }
-
-# Invoke function
-test <- read(raw_folder = file.path("dev", "testing"))
-
-# Check output
-str(test)
-
-# Clear environment / collect garbage
-rm(list = ls()); gc()
-
-# End ----
