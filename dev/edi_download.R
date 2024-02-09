61d45af20a8b60f96f59a30c9dd06932a3e10211
njlyon0 <nicholasjlyon@gmail.com>
2024-02-09 13:56:25 -0500

    Fleshing out first pass at function variant of download!

--- dev/edi_download.R
+++ dev/edi_download.R
@@ -28,18 +28,96 @@ dir.create(path = file.path("dev", "edi"), showWarnings = F)
 # Function Var. ----
 ## --------------- ##
 
+# Define function
+edi_download <- function(package_id = NULL, folder = NULL, quiet = FALSE){
+  
+  # Error out for missing package ID or folder path
+  if(is.null(package_id) == TRUE | is.null(folder) == TRUE)
+    stop("Package ID and destination folder path must be supplied")
+  
+  # Error out for incorrect class of package ID object
+  if(is.character(package_id) != TRUE)
+    stop("Package ID must be supplied as a character")
+  
+  # Error out for malformed package ID
+  ## NEED TO ASK EDI MORE ON THIS TOPIC
+  ## I.e., need to know what rules there are for these IDs to be able to recognize deviations from those rules
+  
+  if(is.logical(quiet) != TRUE){
+    quiet <- FALSE
+    message("`quiet` argument must be set to a logical, defaulting to FALSE") }
+  
+  # Add the package ID to the end of the folder path
+  path <- file.path(folder, package_id)
+  
+  # Create a folder for the package
+  dir.create(path = path, showWarnings = F)
+  
+  # Identify the PASTA identifier
+  pasta_ident <- paste0("https://pasta.lternet.edu/package/eml/",
+                        gsub(pattern = "\\.", replacement = "/", x = package_id))
+  
+  # Identify the products within that package
+  products <- read.csv(file = url(description = pasta_ident), header = F) %>%
+    ## Dropping the identifier we just used
+    dplyr::filter(V1 != pasta_ident)
+  
+  # If `quiet` isn't `TRUE`, print a message about how many files there are
+  if(quiet != TRUE){
+    message(length(products[, 1]), " files identified in specified data package")
+  }
+  
+  # Loop across the number of files in the package
+  for(k in 1:length(products[, 1])){
+    
+    # Generate a temporary file name
+    temp_name <- tempfile()
+    
+    # GET information on that link (needed later)
+    link_info <- httr::GET(url = products[k, 1])
+    
+    # Attempt a download with the 'curl' method
+    try(utils::download.file(url = products[k, 1], destfile = temp_name,
+                             method = "curl", quiet = quiet))
+    
+    # Download with the 'auto' method if the other one didn't work
+    if(is.na(file.size(temp_name)) == TRUE){
+      utils::download.file(url = products[k, 1], destfile = temp_name,
+                           method = "auto", quiet = quiet) }
+    
+    # Identify the file's actual extension
+    type <- stringr::str_extract(string = link_info$all_headers[[1]]$headers$`content-type`,
+                                 pattern = "/[:alnum:]{3,10}")
+    
+    # Swap slash for period
+    ext <- gsub(pattern = "\\/", replacement = ".", x = type)
+    
+    # Generate a nice(r) file name for that
+    real_name <- paste0(package_id, "-file-", k, ext)
+    
+    # Move that file where we want it do be
+    file.rename(from = temp_name, to = file.path(path, real_name))
+    
+  } # Close loop 
+}
 
+# Invoke function
+edi_download(package_id = "edi.1210.1", folder = file.path("dev", "edi"))
+
+# Other tests of function on different data products
+edi_download(package_id = "knb-lter-bes.52.600", folder = file.path("dev", "edi"))
+edi_download(package_id = "knb-lter-and.4544.4", folder = file.path("dev", "edi"))
+edi_download(package_id = "knb-lter-knz.180.2", folder = file.path("dev", "edi"))
+
+# Clear environment
+rm(list = ls())
 
 ## --------------- ##
 # Script Variant ----
 ## --------------- ##
 
-# Define some Package IDs
-package_ids <- c("edi.1210.1", "knb-lter-bes.52.600",
-                 "knb-lter-and.4544.4", "knb-lter-knz.180.2")
-
 # Pick one
-id <- unique(package_ids)[1]
+id <- "edi.1210.1"
 
 # Generate a file path for the package
 folder <- file.path("dev", "edi", id)
