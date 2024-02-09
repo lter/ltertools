34d61deb4c9cf3da518ca9e61d7f009abad300d1
njlyon0 <nicholasjlyon@gmail.com>
2024-02-09 13:34:20 -0500

    Got a variant that works!!! (at least for CSVs)

--- dev/edi_download.R
+++ dev/edi_download.R
@@ -19,19 +19,19 @@ rm(list = ls())
 # EDI Method ----
 ## --------------- ##
 
-# Define URL
-inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/1210/1/c71bb3c137607da2d03d06342f9f1cad" 
-
-# Create tempfile
-infile1 <- tempfile()
-
-# Try one mode of downloading
-try(download.file(url = inUrl1, destfile = infile1, method = "curl"))
-
-# Try another if that doesn't work
-if(is.na(file.size(infile1))) 
-  download.file(url = inUrl1, destfile = infile1, method = "auto")
-
+# # Define URL
+# inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/1210/1/c71bb3c137607da2d03d06342f9f1cad" 
+# 
+# # Create tempfile
+# infile1 <- tempfile()
+# 
+# # Try one mode of downloading
+# try(download.file(url = inUrl1, destfile = infile1, method = "curl"))
+# 
+# # Try another if that doesn't work
+# if(is.na(file.size(infile1))) 
+#   download.file(url = inUrl1, destfile = infile1, method = "auto")
+# 
 
 ## --------------- ##
 # Script Variant ----
@@ -64,29 +64,54 @@ pasta_ident <- paste0("https://pasta.lternet.edu/package/eml/",
                       gsub(pattern = "\\.", replacement = "/", x = id))
 
 # Identify the products within that package
-products <- read.csv(file = url(description = pasta_ident), header = F)
+products <- read.csv(file = url(description = pasta_ident), header = F) %>%
+  ## Dropping the identifier we just used
+  dplyr::filter(V1 != pasta_ident)
 
 # Loop across the number of files in the package
-for(k in length(products[1,])){
+for(k in 1){
+# for(k in length(products[1,])){
   
-  # Generate a nice(r) file name for that
-  # file <- paste0(id, "_file-", k)
-  file <- tempfile()
+  # Generate a temporary file name
+  temp_name <- tempfile()
+  
+  # GET information on that link (needed later)
+  link_info <- httr::GET(url = products[1, k])
   
   # Attempt a download one way
-  try(utils::download.file(url = products[1, k], destfile = file.path(folder, file),
+  try(utils::download.file(url = products[1, k], destfile = temp_name,
                            method = "curl", quiet = FALSE))
   
-  # Download with 'auto' if the other one didn't work
-  if(is.na(file.size(file.path(folder, file))) == TRUE){
-    utils::download.file(url = products[1, k], destfile = file.path(folder, file),
+  # Download with the 'auto' method if the other one didn't work
+  if(is.na(file.size(temp_name)) == TRUE){
+    utils::download.file(url = products[1, k], destfile = temp_name,
                          method = "auto", quiet = FALSE) }
+  
+  # Identify the file's actual extension
+  type <- stringr::str_extract(string = link_info$all_headers[[1]]$headers$`content-type`,
+                               pattern = "/[:alnum:]{3,10}")
+  
+  # Swap slash for period
+  ext <- gsub(pattern = "\\/", replacement = ".", x = type)
+  
+  # Generate a nice(r) file name for that
+  real_name <- paste0(id, "-file-", k, ext)
+  
+  # Move that file where we want it do be
+  file.rename(from = temp_name, to = file.path(folder, real_name))
   
 } # Close loop
 
 
+test <- httr::GET(url = "https://pasta.lternet.edu/package/data/eml/edi/1210/1/c71bb3c137607da2d03d06342f9f1cad")
+
+str(test)
+
+test$all_headers[[1]]$headers$`content-type`
 
 
+
+httr::GET(url = "https://pasta.lternet.edu/package/data/eml/knb-lter-bes/52/600/9e734d0cbec91202f2442edb8a3399d4")
 
 
 
