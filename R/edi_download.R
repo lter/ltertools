21ee360e11ebb353133b4a2047c581d166b80a0c
njlyon0 <nicholasjlyon@gmail.com>
2024-02-09 10:54:32 -0500

    Adding core script structure

---
+++ R/edi_download.R
@@ -0,0 +1,23 @@
+## ------------------------------------- ##
+# Experiment with Downloading EDI Data
+## ------------------------------------- ##
+# Purpose:
+## Want to make a tool that lets users download *any* EDI dataset
+## Using the PASTA identifier makes sense as an initial stab
+
+## --------------- ##
+# Housekeeping ----
+## --------------- ##
+
+
+## --------------- ##
+# Script Variant ----
+## --------------- ##
+
+
+## --------------- ##
+# Function Var. ----
+## --------------- ##
+
+
+# End ----
