0471c5de1c45d0a6bb39f0b2d22922024dd22acf
njlyon0 <nicholasjlyon@gmail.com>
2024-02-09 10:59:47 -0500

    Fleshing out some early housekeeping / set up steps

--- R/edi_download.R
+++ R/edi_download.R
@@ -9,10 +9,31 @@
 # Housekeeping ----
 ## --------------- ##
 
+# Load needed libraries
+librarian::shelf(tidyverse, devtools)
+
+# Clear environment
+rm(list = ls())
 
 ## --------------- ##
 # Script Variant ----
 ## --------------- ##
+
+# Define a local folder for downloading
+folder <- file.path("dev", "edi")
+
+# Create that folder if it doesn't exist
+dir.create(path = folder, showWarnings = F)
+
+# Identify a data package
+## Bees: https://portal.edirepository.org/nis/mapbrowse?packageid=edi.1210.1
+edi_link <- "https://portal.edirepository.org/nis/mapbrowse?packageid=edi.1210.1"
+
+
+# Attempt the download
+# utils::download.file(url = pasta_id, destfile = temp_dest,
+#                      method = "auto", quiet = TRUE)
+
 
 
 ## --------------- ##
