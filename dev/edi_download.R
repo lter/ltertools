1faf32ce2ec2da4bb9012ecce3f826c8e4702d4b
njlyon0 <nicholasjlyon@gmail.com>
2024-02-09 13:38:04 -0500

    Deleting some exploratory methods that didn't pan out

--- dev/edi_download.R
+++ dev/edi_download.R
@@ -16,24 +16,6 @@ librarian::shelf(tidyverse, devtools)
 rm(list = ls())
 
 ## --------------- ##
-# EDI Method ----
-## --------------- ##
-
-# # Define URL
-# inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/1210/1/c71bb3c137607da2d03d06342f9f1cad" 
-# 
-# # Create tempfile
-# infile1 <- tempfile()
-# 
-# # Try one mode of downloading
-# try(download.file(url = inUrl1, destfile = infile1, method = "curl"))
-# 
-# # Try another if that doesn't work
-# if(is.na(file.size(infile1))) 
-#   download.file(url = inUrl1, destfile = infile1, method = "auto")
-# 
-
-## --------------- ##
 # Script Variant ----
 ## --------------- ##
 
@@ -101,83 +83,6 @@ for(k in 1:length(products[, 1])){
   file.rename(from = temp_name, to = file.path(folder, real_name))
   
 } # Close loop
-
-
-test <- httr::GET(url = "https://pasta.lternet.edu/package/data/eml/edi/1210/1/c71bb3c137607da2d03d06342f9f1cad")
-
-str(test)
-
-test$all_headers[[1]]$headers$`content-type`
-
-
-
-httr::GET(url = "https://pasta.lternet.edu/package/data/eml/knb-lter-bes/52/600/9e734d0cbec91202f2442edb8a3399d4")
-
-
-
-# Define links
-edi_links <- c("https://portal.edirepository.org/nis/mapbrowse?packageid=edi.1210.1", "https://portal.edirepository.org/nis/mapbrowse?scope=knb-lter-bes&identifier=52", "https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-and.4544.4", "https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-knz.180.2")
-
-# Make a copy for later
-pasta_ids <- edi_links
-
-# Loop across that vector
-for(link in edi_links){
-  
-  # Deduce the PASTA idenfitier from the search link
-  if(stringr::str_detect(string = link, pattern = "\\?packageid\\=")){
-    
-    # Remove that link from the PASTA identifier vector
-    pasta_ids <- setdiff(x = pasta_ids, y = link)
-    
-    # Drop rest of URL
-    id_v1 <- gsub(pattern = "https\\:\\/\\/portal.edirepository.org\\/nis\\/mapbrowse\\?packageid\\=", replacement = "", x = link)
-    
-    # Replace periods with slashes
-    id_v2 <- gsub(pattern = "\\.", replacement = "/", x = id_v1)
-    
-    # Re-assemble into PASTA identifier
-    pasta_id <- paste0("https://pasta.lternet.edu/package/eml/", id_v2)
-    
-    # Add the finished ID back onto the vector of IDs
-    pasta_ids <- c(pasta_ids, pasta_id) }
-  
-  # Deduce the PASTA identifier from the *identifier* link
-  if(stringr::str_detect(string = link, pattern = "\\&identifier\\=")){
-    
-    # Remove that link from the PASTA identifier vector
-    pasta_ids <- setdiff(x = pasta_ids, y = link)
-    
-    # Replace ampersand with a slash
-    id_v1 <- gsub(pattern = "\\&", replacement = "/", x = link)
-    
-    # Drop rest of URL
-    id_v2 <- gsub(pattern = "https\\:\\/\\/portal.edirepository.org\\/nis\\/mapbrowse\\?scope\\=|identifier\\=", replacement = "", x = id_v1)
-    
-    # Replace periods with slashes
-    # id_v3 <- gsub(pattern = "\\.", replacement = "/", x = id_v2)
-    
-    # Re-assemble into PASTA identifier
-    pasta_id <- paste0("https://pasta.lternet.edu/package/eml/", id_v2)
-    
-    # Add the finished ID back onto the vector of IDs
-    pasta_ids <- c(pasta_ids, pasta_id)
-    
-  }
-} # Close loop
-
-# Check what that returns
-pasta_ids
-
-
-
-
-edi_link <- "https://portal.edirepository.org/nis/mapbrowse?packageid=edi.1210.1"
-edi_pasta <- "https://pasta.lternet.edu/package/eml/edi/1210/1"
-
-# Attempt the download
-# utils::download.file(url = pasta_id, destfile = temp_dest,
-#                      method = "auto", quiet = TRUE)
 
 
 
