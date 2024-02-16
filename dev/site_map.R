8c080cf95492c8336c2d18698898a7e10e8a60e9
njlyon0 <nicholasjlyon@gmail.com>
2024-02-16 11:29:53 -0500

    Fleshed out "regional" maps that allow for better visualization of sites

--- dev/site_map.R
+++ dev/site_map.R
@@ -1,17 +1,155 @@
 ## ------------------------------ ##
 # Site Map ----
 ## ------------------------------ ##
+# Load libraries
+library(tidyverse); devtools::load_all()
 
-library(magrittr)
+# Clear environment
+rm(list = ls())
 
 # Subset to desired sites
 site_info <- site_subset()
 dplyr::glimpse(site_info)
 
+# Custom color palette (by habitat)
 habitat_colors <- c("Admin" = "#fcbf49", "Urban" = "#f77f00",
                     "Marine" = "#0466c8", "Coastal" = "#34a0a4", "Freshwater" = "#8ecae6", 
                     "Forest" = "#007200", "Grassland" = "#70e000", 
                     "Mixed" = "#9d4edd", "Tundra" = "#bb9457")
+
+# Get an object of world / US state borders
+borders <- sf::st_as_sf(maps::map(database = "world", plot = F, fill = T)) %>%
+  dplyr::bind_rows(sf::st_as_sf(maps::map(database = "state", plot = F, fill = T)))
+
+# Consolidate some ggplot formatting elements
+lter_map_theme <-  theme_bw() + 
+  theme(axis.title = element_blank(),
+        axis.text = element_text(size = 12),
+        legend.title = element_blank())
+
+## ------------------------------ ##
+# Moorea Map (MCR) ----
+## ------------------------------ ##
+# Check coordinates
+dplyr::filter(site_info, code == "MCR")
+
+# Make map
+mcr_map <- borders %>%
+  ggplot() +
+  geom_sf(fill = "gray95") +
+  # Set map extent
+  coord_sf(xlim = c(-160, -100), ylim = c(-25, 40), expand = F) +
+  # Add points & labels for LTER sites
+  geom_point(data = site_info, aes(x = longitude, y = latitude, fill = habitat), 
+             pch = 21, size = 10) +
+  # Customize color
+  scale_fill_manual(values = habitat_colors) +
+  # Customize axis labels
+  labs(x = "Longitude", y = "Latitude") +
+  # Tweak theme / formatting
+  lter_map_theme +
+  theme(legend.position = "none"); mcr_map
+
+## ------------------------------ ##
+  # Antarctica Map (MCM + PAL) ----
+## ------------------------------ ##
+# Check coordinates
+dplyr::filter(site_info, code %in% c("MCM", "PAL"))
+
+# Make map
+south_map <- borders %>%
+  ggplot() +
+  geom_sf(fill = "gray95") +
+  # Set map extent
+  coord_sf(xlim = c(-80, 170), ylim = c(-80, -60), expand = F) +
+  # Add points & labels for LTER sites
+  geom_point(data = site_info, aes(x = longitude, y = latitude, fill = habitat), 
+             pch = 21, size = 10) +
+  # Customize color
+  scale_fill_manual(values = habitat_colors) +
+  # Customize axis labels
+  labs(x = "Longitude", y = "Latitude") +
+  # Tweak theme / formatting
+  lter_map_theme +
+  theme(legend.position = "none"); south_map
+
+## ------------------------------ ##
+          # Alaska Map ----
+## ------------------------------ ##
+# Check coordinates
+dplyr::filter(site_info, code %in% c("BLE", "ARC", "BNZ", "NGA"))
+
+# Make map
+ak_map <- borders %>%
+  ggplot() +
+  geom_sf(fill = "gray95") +
+  # Set map extent
+  coord_sf(xlim = c(-170, -130), ylim = c(58, 72), expand = F) +
+  # Add points & labels for LTER sites
+  geom_point(data = site_info, aes(x = longitude, y = latitude, fill = habitat), 
+             pch = 21, size = 10) +
+  # Customize color
+  scale_fill_manual(values = habitat_colors) +
+  # Customize axis labels
+  labs(x = "Longitude", y = "Latitude") +
+  # Tweak theme / formatting
+  lter_map_theme +
+  theme(legend.position = "none"); ak_map
+
+## ------------------------------ ##
+# Puerto Rico Map (LUQ) ----
+## ------------------------------ ##
+# Check coordinates
+dplyr::filter(site_info, code == "LUQ")
+
+# Make map
+luq_map <- borders %>%
+  ggplot() +
+  geom_sf(fill = "gray95") +
+  # Set map extent
+  coord_sf(xlim = c(-90, -60), ylim = c(10, 35), expand = F) +
+  # Add points & labels for LTER sites
+  geom_point(data = site_info, aes(x = longitude, y = latitude, fill = habitat), 
+             pch = 21, size = 10) +
+  # Customize color
+  scale_fill_manual(values = habitat_colors) +
+  # Customize axis labels
+  labs(x = "Longitude", y = "Latitude") +
+  # Tweak theme / formatting
+  lter_map_theme +
+  theme(legend.position = "none"); luq_map
+
+## ------------------------------ ##
+# CONUS Map ----
+## ------------------------------ ##
+# CONUS == Continental US
+
+# Check coordinates
+dplyr::filter(site_info, !code %in% c("MCR", "MCM", "PAL", "LUQ",
+                                      "BLE", "ARC", "BNZ", "NGA"))
+
+# Make map
+usa_map <- borders %>%
+  ggplot() +
+  geom_sf(fill = "gray95") +
+  # Set map extent
+  coord_sf(xlim = c(-125, -68), ylim = c(25, 48), expand = F) +
+  # Add points & labels for LTER sites
+  geom_point(data = site_info, aes(x = longitude, y = latitude, fill = habitat), 
+             pch = 21, size = 6) +
+  # Customize color
+  scale_fill_manual(values = habitat_colors) +
+  # Customize axis labels
+  labs(x = "Longitude", y = "Latitude") +
+  # Tweak theme / formatting
+  lter_map_theme +
+  theme(legend.position = "none"); usa_map
+
+
+## ------------------------------ ##
+# Global Map ----
+## ------------------------------ ##
+
 
 # Identify min & max latitude / longitude
 (min_lat <- min(site_info$lat, na.rm = T))
@@ -23,9 +161,6 @@ habitat_colors <- c("Admin" = "#fcbf49", "Urban" = "#f77f00",
 (lat_lims <- c((min_lat - 0.1 * min_lat), (max_lat + 0.1 * max_lat)))
 (lon_lims <- c((min_lon + 0.15 * min_lon), (max_lon - 0.15 * max_lon)))
 
-# Get an object of world / US state borders
-borders <- sf::st_as_sf(maps::map(database = "world", plot = F, fill = T)) %>%
-  dplyr::bind_rows(sf::st_as_sf(maps::map(database = "state", plot = F, fill = T)))
 
 # Make map
 map <- borders %>%
@@ -44,9 +179,5 @@ map <- borders %>%
   # Customize axis labels
   labs(x = "Longitude", y = "Latitude") +
   # Tweak theme / formatting
-  theme_bw() + 
-  theme(panel.border = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_text(size = 12),
-        legend.title = element_blank()); map
+  lter_map_theme; map
 
