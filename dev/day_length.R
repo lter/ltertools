c1b089fa70a1cb1c367e3f155e73f13e49a034c9
njlyon0 <nicholasjlyon@gmail.com>
2024-03-07 09:34:52 -0500

    Script skeleton for day length function

---
+++ dev/day_length.R
@@ -0,0 +1,30 @@
+## ----------------------------------- ##
+# Day Length
+## ----------------------------------- ##
+
+# PURPOSE:
+## Identify day length for every day between two dates at a specified set of coordinates
+
+## --------------------- ##
+    # Housekeeping ----
+## --------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+## --------------------- ##
+# Script Variant ----
+## --------------------- ##
+
+
+
+## --------------------- ##
+# Function Variant ----
+## --------------------- ##
+
+
+
+# End ----
