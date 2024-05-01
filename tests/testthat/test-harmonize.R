3d6b0a455169ac5418b3aaf1c9e14d491b54bf18
njlyon0 <nicholasjlyon@gmail.com>
2024-05-01 12:28:30 -0400

    Commenting out tests I haven't gotten to yet

--- tests/testthat/test-harmonize.R
+++ tests/testthat/test-harmonize.R
@@ -1,14 +1,15 @@
-# `harmonize` Tests ----
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-harmonize.R"))
 
-# Simple error testing
-test_that("Only accepts correct inputs", {
-  expect_error()
-  
-  
-})
-
-# Output testing
-test_that("Outputs are correct", {
-  expect_equal(, "xx")
-})
-
+# # Simple error testing
+# test_that("Only accepts correct inputs", {
+#   expect_error()
+#   
+#   
+# })
+# 
+# # Output testing
+# test_that("Outputs are correct", {
+#   expect_equal(, "xx")
+# })
+# 
--- tests/testthat/test-site_timeline.R
+++ tests/testthat/test-site_timeline.R
@@ -1,14 +1,15 @@
-# `begin_key` Tests ----
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-site_timeline.R"))
 
-# Simple error testing
-test_that("Only accepts correct inputs", {
-  expect_error()
-  
-  
-})
-
-# Output testing
-test_that("Outputs are correct", {
-  expect_equal(, "xx")
-})
+# # Simple error testing
+# test_that("Only accepts correct inputs", {
+#   expect_error()
+#   
+#   
+# })
+# 
+# # Output testing
+# test_that("Outputs are correct", {
+#   expect_equal(, "xx")
+# })
 
