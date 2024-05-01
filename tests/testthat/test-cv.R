c1bbb1f4b05e791cb24ee98fe8ff0c13ca77bebd
njlyon0 <nicholasjlyon@gmail.com>
2024-05-01 12:00:22 -0400

    First pass at `cv` testing script

--- tests/testthat/test-cv.R
+++ tests/testthat/test-cv.R
@@ -1,14 +1,18 @@
-# `cv` Tests ----
+# Test this function:
+## test_file(file.path("tests", "testthat", "test-cv.R"))
 
 # Simple error testing
 test_that("Only accepts correct inputs", {
-  expect_error()
-  
-  
+  expect_error(cv(x = c("x", "y", "z"), na_rm = TRUE))
+})
+
+# Warning testing
+test_that("Warnings are returned",{
+  expect_warning(cv(x = c(1:10), na_rm = "false"))
 })
 
 # Output testing
 test_that("Outputs are correct", {
-  expect_equal(, "xx")
+  expect_equal(cv(x = c(1:10), na_rm = TRUE), 0.55048188)
 })
 
