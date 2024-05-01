682dab63632419ef35d125989b43fb6e779ce43f
njlyon0 <nicholasjlyon@gmail.com>
2024-05-01 11:53:26 -0400

    First pass at `solar_day_info` tests

--- tests/testthat/test-solar_day_info.R
+++ tests/testthat/test-solar_day_info.R
@@ -1,14 +1,35 @@
-# `solar_day_info` Tests ----
-
 # Simple error testing
 test_that("Only accepts correct inputs", {
-  expect_error()
-  
-  
+  expect_error(solar_day_info(lat = 4000, lon = -119, 
+                              start_date = "2022-02-07", 
+                              end_date = "2022-02-08", quiet = TRUE))
+  expect_error(solar_day_info(lat = 34, lon = 4000, 
+                              start_date = "2022-02-07", 
+                              end_date = "2022-02-08", quiet = TRUE))
+  expect_error(solar_day_info(lat = 34, lon = -119, 
+                              start_date = "feb 7, 2022", 
+                              end_date = "2022-02-08", quiet = TRUE))
+  expect_error(solar_day_info(lat = 34, lon = -119, 
+                              start_date = "2022-02-07", 
+                              end_date = "2022/02/08", quiet = TRUE))
+  expect_error(solar_day_info(lat = 34, lon = -119, 
+                              start_date = "2022-02-07", 
+                              end_date = "22-02-08", quiet = TRUE))
+})
+
+# Warning testing
+test_that("Warnings are returned",{
+  expect_warning(solar_day_info(lat = 34, lon = -119, 
+                                start_date = "2022-02-07", end_date = "2022-02-08", 
+                                quiet = "xxx"))
 })
 
 # Output testing
 test_that("Outputs are correct", {
-  expect_equal(, "xx")
+  
+  sun_faq <- solar_day_info(lat = 34, lon = -119, 
+                            start_date = "2022-02-07", end_date = "2022-02-08", 
+                            quiet = TRUE)
+  
+  expect_equal(class(sun_faq), "data.frame")
 })
-
