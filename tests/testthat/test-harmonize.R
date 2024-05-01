885f6490f7709d01a7e4f12de9689fbe85d07f37
njlyon0 <nicholasjlyon@gmail.com>
2024-05-01 11:26:12 -0400

    First commit of testing skeletons for all functions in package

---
+++ tests/testthat/test-harmonize.R
@@ -0,0 +1,14 @@
+# `harmonize` Tests ----
+
+# Simple error testing
+test_that("Only accepts correct inputs", {
+  expect_error()
+  
+  
+})
+
+# Output testing
+test_that("Outputs are correct", {
+  expect_equal(, "xx")
+})
+
---
+++ tests/testthat/test-solar_day_info.R
@@ -0,0 +1,14 @@
+# `solar_day_info` Tests ----
+
+# Simple error testing
+test_that("Only accepts correct inputs", {
+  expect_error()
+  
+  
+})
+
+# Output testing
+test_that("Outputs are correct", {
+  expect_equal(, "xx")
+})
+
