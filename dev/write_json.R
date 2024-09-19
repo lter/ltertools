84291ddee366b0afd5057b88f790f206ac73861d
njlyon0 <nicholasjlyon@gmail.com>
2024-09-19 11:33:44 -0700

    feat: script skeleton for `write_json` function exploration

---
+++ dev/write_json.R
@@ -0,0 +1,23 @@
+## --------------------------------------------------- ##
+                    # Write JSON Function
+## --------------------------------------------------- ##
+# Author(s): Nick J Lyon
+
+# Purpose:
+## We want a function that makes (simple) JSON files that contain user-defined content
+## Useful for housing user-specific information (incl. absolute file paths, emails, etc.)
+
+## ----------------------------------- ##
+# Exploration ----
+## ----------------------------------- ##
+
+
+
+
+
+
+
+
+
+
+# End ----
