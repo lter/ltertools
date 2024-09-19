3273952ddbca6a12f0c3a32ebe4ec8958ff7a30e
njlyon0 <nicholasjlyon@gmail.com>
2024-09-19 12:01:32 -0700

    feat: first pass at function variant

--- dev/write_json.R
+++ dev/write_json.R
@@ -7,12 +7,12 @@
 ## We want a function that makes (simple) JSON files that contain user-defined content
 ## Useful for housing user-specific information (incl. absolute file paths, emails, etc.)
 
+# Load libraries
+librarian::shelf(jsonlite, RJSONIO)
+
 ## ----------------------------------- ##
 # Exploration ----
 ## ----------------------------------- ##
-
-# Load libraries
-librarian::shelf(jsonlite, RJSONIO)
 
 # Clear environment
 rm(list = ls())
@@ -29,5 +29,40 @@ write(x = contents_json, file = file.path("dev", "test.json"))
 # Read in JSON and see if it looks 'right'
 user_info <- jsonlite::read_json(file.path("dev", "test.json"))
 str(user_info)
+
+## ----------------------------------- ##
+# Function Dev ----
+## ----------------------------------- ##
+
+# Clear environment
+rm(list = ls())
+
+# Define function
+make_json <- function(x = NULL, file = NULL){
+  
+  # Error for null arguments
+  # Error for non-character arguments
+  # Error for different length of names/values
+  # Error for non-unique names
+  # Warning for 'file' not ending in '.json'
+  
+  # Transform contents to JSON format
+  json_content <- RJSONIO::toJSON(x = x)
+  
+  # Export
+  write(x = json_content, file = file)
+  
+}
+
+# Invoke function
+make_json(x = c("greeting" = "hello", "farewell" = "goodbye"),
+          file = file.path("dev", "test.json"))
+
+# Read in JSON and see if it looks 'right'
+(user_info <- jsonlite::read_json(file.path("dev", "test.json")))
+
+user_info$farewell
+
+
 
 # End ----
