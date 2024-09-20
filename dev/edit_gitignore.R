89ec775bda45cf5e562545e91ea07450f8c12254
njlyon0 <nicholasjlyon@gmail.com>
2024-09-20 09:34:17 -0700

    feat: script skeleton for figuring out how to edit the gitignore

---
+++ dev/edit_gitignore.R
@@ -0,0 +1,20 @@
+## -------------------------------------------- ##
+                # Edit `.gitignore`
+## -------------------------------------------- ##
+# Author(s): Nick J Lyon
+
+# Purpose:
+## Edit (add to) the `.gitignore` via an R function
+
+# Load libraries
+librarian::shelf(devtools)
+
+## ----------------------------- ##
+# Explore ----
+## ----------------------------- ##
+
+# Clear environment
+rm(list = ls())
+
+
+# End ----
