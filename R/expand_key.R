97be48fd037b980aeca7b8c58c82a50391fae407
njlyon0 <nicholasjlyon@gmail.com>
2025-02-20 16:49:58 -0500

    docs: added example to `expand_key` documentation

--- R/expand_key.R
+++ R/expand_key.R
@@ -14,6 +14,43 @@
 #' 
 #' @export
 #'  
+#' @examples
+#' # Generate two simple tables
+#' ## Dataframe 1
+#' df1 <- data.frame("xx" = c(1:3),
+#'                   "unwanted" = c("not", "needed", "column"),
+#'                   "yy" = letters[1:3])
+#' ## Dataframe 2
+#' df2 <- data.frame("LETTERS" = letters[4:7],
+#'                   "NUMBERS" = c(4:7),
+#'                   "BONUS" = c("plantae", "animalia", "fungi", "protista"))
+#' 
+#' # Generate a local folder for exporting
+#' temp_folder <- tempdir()
+#' 
+#' # Export both files to that folder
+#' utils::write.csv(x = df1, file = file.path(temp_folder, "df1.csv"), row.names = FALSE)
+#' utils::write.csv(x = df2, file = file.path(temp_folder, "df2.csv"), row.names = FALSE)
+#' 
+#' # Generate a column key with "guesses" at tidy column names
+#' key1 <- ltertools::begin_key(raw_folder = temp_folder, data_format = "csv", guess_tidy = TRUE)
+#' 
+#' # Export this locally to the temp folder
+#' utils::write.csv(x = key1, file = file.path(temp_folder, "key1.csv"), row.names = FALSE)
+#' 
+#' # Harmonize the data
+#' harmony <- ltertools::harmonize(key = key1, raw_folder = temp_folder)
+#' 
+#' # Make a new data file
+#' df3 <- data.frame("xx" = c(10:15),
+#'                   "letters" = letters[10:15])
+#' 
+#' # Export this locally to the temp folder too
+#' utils::write.csv(x = df3, file = file.path(temp_folder, "df3.csv"), row.names = FALSE)
+#' 
+#' # Identify what needs to be added to the existing column key
+#' ltertools::expand_key(key = key1, raw_folder = temp_folder, harmonized_df = harmony,
+#'                       data_format = "csv", guess_tidy = TRUE)
 #' 
 expand_key <- function(key = NULL, raw_folder = NULL, harmonized_df = NULL, 
                        data_format = c("csv", "txt", "xls", "xlsx"), guess_tidy = FALSE){
