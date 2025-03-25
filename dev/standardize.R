6db2e03ea0eea5be46496b3b5c075b48b4393a32
njlyon0 <nicholasjlyon@gmail.com>
2025-03-25 16:39:32 -0400

    dev: exploring `standardize` sub-function of `harmonize`

---
+++ dev/standardize.R
@@ -0,0 +1,61 @@
+
+# Load needed libraries
+librarian::shelf(tidyverse, supportR)
+
+# Clear environment
+rm(list = ls()); gc()
+
+# Read in key
+key_obj <- read.csv(file = file.path("dev", "key.csv"))
+dplyr::glimpse(key_obj)
+
+# Read in some test data files
+data_list <- ltertools::read(raw_folder = file.path("dev", "test-data"), data_format = "csv")
+dplyr::glimpse(data_list[[1]])
+
+# Make comparable list of key elements
+key_list <- purrr::map(.x = names(data_list), 
+                       .f = ~ dplyr::filter(key_obj, source == .x)) 
+dplyr::glimpse(key_list[[1]])
+
+
+
+# Define function
+standardize <- function(focal_file = NULL, key = NULL, df_list = NULL){
+  
+  # Grab single key/data component
+  focal_key <- dplyr::filter(key, source == focal_file)
+  focal.df_orig <- df_list[[focal_file]]
+  
+  # Identify any columns in the column key but apparently not in the data
+  missing_cols <- base::setdiff(x = unique(focal_key$raw_name),
+                                y = names(focal.df_orig))
+  
+  # Warn the user if any are found (this is a warning so no `quiet` argument used)
+  if(length(missing_cols) > 0){
+    warning(message = paste0("Following columns in key NOT found in '", focal_file, "': '", missing_cols, "'", collapse = " & ")) }
+  
+  # Standardize this dataset
+  focal.df_std <- focal.df_orig %>% 
+    # Keep only columns with tidy equivalents
+    dplyr::select(dplyr::all_of(unique(focal_key$raw_name))) %>% 
+    # Make all columns characters
+    dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
+                                .fns = as.character)) %>% 
+    # Add a filename column
+    dplyr::mutate(source = focal_file, .before = dplyr::everything()) %>% 
+    # Standardize names with key
+    supportR::safe_rename(data = ., bad_names = focal_key$raw_name,
+                          good_names = focal_key$tidy_name)
+  
+  # Return the standard object
+  return(focal.df_std) }
+
+# Invoke function
+test_std <- standardize(key = key_obj, df_list = data_list, focal_file = names(df_list[1]))
+dplyr::glimpse(test_std)
+
+
+
+# End ----
+
